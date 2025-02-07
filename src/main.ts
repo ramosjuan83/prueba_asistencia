9212490915a22ad82630cc81f9e9d9010a2c499f
Juan <juan21ramos@gmail.com>
2025-02-07 12:16:21 -0400

    modificacion de cors

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //app.enableCors();
 
-  // app.enableCors({
-  //   origin: [
-  //     'http://localhost:3000',
-  //     'http://example.com',
-  //     'http://www.example.com',
-  //     'http://app.example.com',
-  //     'https://example.com',
-  //     'https://www.example.com',
-  //     'https://app.example.com',
-  //   ],
-  //   methods: ["GET", "POST"],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://10.11.0.227:3000',
+    ],
+    methods: ["GET", "POST",'DELETE','PUT'],
+    credentials: true,
+  });
+
   
   await app.listen(process.env.PORT ?? 3000);
 }
